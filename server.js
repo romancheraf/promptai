eb1c17e51999faa8dc4324d821c34fe6e628be79
romancheraf <139028718+romancheraf@users.noreply.github.com>
2023-07-09 12:45:15 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,21 @@
-const API_KEY = "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"
+const API_KEY = "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";
 
-async function fetchData() {      
-   const response = await fetch("https://api.openai.com/v1/completions", {
-     method: "POST",
-     headers: {
-         Authorization: 'Bearer ${API_KEY}',
-         "Content-Type": "application/json"
-     },
-     body: JSON.stringfy({
-       mode: "text-davinci-003"
-       prompt: "hellow,how are you today?"
-       max_token: 50
-     })
-   })
-  const date = await response.json()
-  console.log(data)
-}         
+async function fetchData() {
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: "Hello, how are you today?",
+      max_tokens: 50
+    })
+  });
+  
+  const data = await response.json();
+  console.log(data);
+}
 
-fetchData()
+fetchData();
